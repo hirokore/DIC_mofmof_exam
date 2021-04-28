d0f803708906bdbce413b5ba438ad508a7cd4145
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-28 11:34:04 +0900

    update: property/nearest_stationモデル作成

---
+++ db/migrate/20210428023216_create_properties.rb
@@ -0,0 +1,12 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.string :value
+      t.string :age
+      t.text :memo
+
+      t.timestamps
+    end
+  end
+end
