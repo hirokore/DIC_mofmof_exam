1a593736112586bdc6ae26933576a26703d48efc
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-28 11:38:13 +0900

    update: routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'propertys/new'
-  get 'propertys/index'
-  get 'propertys/show'
-  get 'propertys/edit'
-  get 'propertys/confirm'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'propertys#index'
+  resources :nearest_stations
+  resources :propertys do
+    collection do
+      post :confirm
+    end
+  end
 end
