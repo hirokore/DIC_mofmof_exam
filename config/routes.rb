7e323d17d9a7e1e0aa327a973c1b772a756e0814
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-28 11:35:44 +0900

    update: propertys/nearest_stationsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'propertys/new'
+  get 'propertys/index'
+  get 'propertys/show'
+  get 'propertys/edit'
+  get 'propertys/confirm'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/propertys_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class PropertysControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get propertys_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get propertys_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get propertys_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get propertys_edit_url
+    assert_response :success
+  end
+
+  test "should get confirm" do
+    get propertys_confirm_url
+    assert_response :success
+  end
+
+end
